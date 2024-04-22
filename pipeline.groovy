7afd96fd4999f8d55a46394294ee00d43b07f47d
ravishaaaan <engravishan@gmail.com>
2024-04-22 19:02:40 +0530

    Update Docker image tags in pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -6,27 +6,17 @@ pipeline {
   stages {
     stage('build'){
       steps {
-        sh 'docker build -t ravishaaaan/4277-node-app-image .'
+        sh 'docker build -t 4277-node-image-jenkins .'
       }
     }
     stage('run'){
       steps{
-        sh 'docker run -d -p 5000:3000 ravishaaaan/4277-node-app-image'
+        sh 'docker run -d -p 5000:3001 4277-node-image-jenkins'
       }
     }
     stage('final'){
       steps{
         sh 'docker ps'
-      }
-    }
-    stage('push'){
-      steps{
-        script{
-          withCredentials([string(credentialsId: 'docker-pwd', variable: 'docker')]) {
-    sh 'docker login -u ravishaaaan -p ${docker}'
-  }
-    sh 'docker push ravishaaaan/4277-node-app-image'
-        }
       }
     }
   }
